2bdd319e8f321d4f1cfab47a9be9d94c26ddadd6
aloheRoblox <zajnakturkbaev@gmail.com>
2026-05-26 22:30:13 +0600

    Update render-afk-bot_index.js

--- render-afk-bot_index.js
+++ render-afk-bot_index.js
@@ -15,7 +15,7 @@ app.listen(PORT, () => {
 
 // 2. Настройки подключения к Майнкрафт серверу
 const botOptions = {
-    host: 'WeAreSmoFun.aternos.me', // Твой айпи Атернос
+    host: 'WeAreSmpFun.aternos.me', // Твой айпи Атернос
     port: 13795,                     // Стандартный порт
     username: 'AFK_Bot',             // Ник бота в игре
     version: '1.21.1'                   // Автоматический выбор версии
